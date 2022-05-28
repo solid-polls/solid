72520c6fb364246d67c4aa03703fdf2d5d750bdb
Richard Wohlbold <richard@rgwohlbold.de>
2022-05-28 10:39:11 +0200

    Add OpenAPI code generation (#2)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'dev') {
+    app.enableCors({ origin: 'http://localhost:8000' });
+  }
+
+  const config = new DocumentBuilder()
+    .setTitle('Solid Polls')
+    .addServer('http://localhost:3000', 'Testing')
+    .addServer('https://solidpolls.de/api', 'Production')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
