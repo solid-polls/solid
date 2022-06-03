9647ebe3844c8a86c9b9d020bfd61a95ec209ea6
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2022-06-03 11:12:38 +0200

    Setup linter in frontend (#4)
    * Setup eslint and prettier
    
    * Include frontend linting in precommit
    
    * Satisfy linter
    
    * Update package-lock
    
    * Remove unnecessary jest from eslint config

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'warn',
+  },
+};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,55 @@
-import {defaultApi} from "./api";
-import {useQuery} from "react-query";
-import {Link, Route, Switch } from "wouter";
-import { useState } from "react";
+import { defaultApi } from './api';
+import { useQuery } from 'react-query';
+import { Link, Route, Switch } from 'wouter';
+import { useState } from 'react';
 
 type PollPageProps = {
-  params: { code: string }
-}
+  params: { code: string };
+};
 
 function PollPage(props: PollPageProps) {
-  const { isLoading, isSuccess, isError, data, error} = useQuery(['poll', props.params.code], () => defaultApi.appControllerGetPoll({code: +props.params.code}));
+  const { isLoading, isSuccess, isError, data, error } = useQuery(
+    ['poll', props.params.code],
+    () => defaultApi.appControllerGetPoll({ code: +props.params.code }),
+  );
 
   return (
-      <div>
-        <h1>Poll Page</h1>
-        {isLoading && <div>Loading ...</div>}
-        {isSuccess && <div>The poll: {JSON.stringify(data)}</div>}
-        {isError && <div>Could not fetch resource: {JSON.stringify(error)}</div>}
-      </div>
+    <div>
+      <h1>Poll Page</h1>
+      {isLoading && <div>Loading ...</div>}
+      {isSuccess && <div>The poll: {JSON.stringify(data)}</div>}
+      {isError && <div>Could not fetch resource: {JSON.stringify(error)}</div>}
+    </div>
   );
 }
 
 function HomePage() {
   const [code, setCode] = useState('');
-  return <div>
-    <h1>Home</h1>
-    <div>Please type in the poll code:</div>
-    <input type="text" onChange={event => setCode(event.target.value)} />
-    <Link href={`/poll/${code}`}>
-      <button type="submit">Join Poll</button>
-    </Link>
-  </div>
+  return (
+    <div>
+      <h1>Home</h1>
+      <div>Please type in the poll code:</div>
+      <input type="text" onChange={(event) => setCode(event.target.value)} />
+      <Link href={`/poll/${code}`}>
+        <button type="submit">Join Poll</button>
+      </Link>
+    </div>
+  );
 }
 
 function App() {
-  return <div>
-  <div><Link href="/">SolidPolls</Link></div>
-  
-  <Switch>
-    <Route path='/poll/:code' component={PollPage} />
-    <Route path='/' component={HomePage} />
-  </Switch>
-</div>;
+  return (
+    <div>
+      <div>
+        <Link href="/">SolidPolls</Link>
+      </div>
+
+      <Switch>
+        <Route path="/poll/:code" component={PollPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
+    </div>
+  );
 }
 
-export default App
+export default App;
