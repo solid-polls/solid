dd6963470be7303a0c3c13366088fe1d2202f2d5
Richard Wohlbold <richard@rgwohlbold.de>
2022-06-16 14:35:50 +0200

    Add type definitions into frontend repo

---
+++ frontend/src/types/voting.d.ts
@@ -0,0 +1,19 @@
+/* Edit this file in backend/src/types */
+
+interface UpdatePayload {
+  votes: number;
+}
+
+interface ServerToClientEvents {
+  update: (payload: UpdatePayload) => void;
+}
+
+interface VotePayload {
+  pollCode: string;
+  questionID: number;
+  answerID: number;
+}
+
+interface ClientToServerEvents {
+  vote: (payload: VotePayload) => void;
+}
