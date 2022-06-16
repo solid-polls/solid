2e81c5b92a5a79867fa6389c222dbbadf4c2dee6
Richard Wohlbold <richard@rgwohlbold.de>
2022-06-16 11:16:12 +0200

    Fix Import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import options from './config/ormconfig';
 import { PollModule } from './poll/poll.module';
-import { VoteGateway } from './vote.gateway';
+import { VoteGateway } from './vote/vote.gateway';
 
 @Module({
   imports: [TypeOrmModule.forRoot(options), PollModule],
