b8f075bfae78a3be5ca087ed3d134cd61af1f728
Aaron Schlitt <aaron@aaronschlitt.de>
2022-06-26 18:24:42 +0200

    Display a graph of voting results

--- frontend/src/components/QuestionResults.tsx
+++ frontend/src/components/QuestionResults.tsx
@@ -1,16 +1,60 @@
-import { Question } from '../client';
-import { Stack, Typography } from '@mui/material';
+import { Answer, Question } from '../client';
+import { Container, Stack } from '@mui/material';
+import Chart from 'react-apexcharts';
 
 interface QuestionResultsProps {
   question: Question;
 }
 
 export default function QuestionResults(props: QuestionResultsProps) {
+  const series = [];
+  const labels = [];
+
+  // The order of these changes on database changes by voting, to maintain a stable chart, we need to sort the answers by name
+  const answers = props.question.answers.sort(answerSorter);
+
+  for (const answer of answers) {
+    series.push(answer.count);
+    labels.push(answer.text);
+  }
+
+  const options = {
+    labels,
+    theme: {
+      mode: 'dark' as const,
+    },
+    chart: {
+      background: 'none',
+      fontFamily: 'Roboto',
+    },
+    legend: {
+      position: 'top' as const,
+    },
+    plotOptions: {
+      pie: {
+        donut: {
+          labels: {
+            show: true,
+            total: {
+              show: true,
+            },
+          },
+        },
+      },
+    },
+  };
+
   return (
     <Stack>
-      <Typography variant='body1'>
-        The question: {JSON.stringify(props.question)}
-      </Typography>
+      <Container maxWidth='sm'>
+        <Chart options={options} series={series} type='donut' width='100%' />
+      </Container>
     </Stack>
   );
+}
+
+function answerSorter(a1: Answer, a2: Answer) {
+  if (a1.text > a2.text) return 1;
+  if (a1.text < a2.text) return -1;
+  return 0;
 }
