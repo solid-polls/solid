45e423f63b03bf335e8bdab2d4da2b7aa4eb795f
Richard Wohlbold <richard@rgwohlbold.de>
2022-06-26 16:15:53 +0200

    Handle non-existing poll more gracefully

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,29 +1,60 @@
 import { useState } from 'react';
-import { Button, Stack, TextField, Typography } from '@mui/material';
-import { Link } from 'wouter';
+import {
+  Alert,
+  Button,
+  Snackbar,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
 import useSetPageTitle from './hooks/useSetPageTitle';
+import { pollsApi } from './api';
+import { useLocation } from 'wouter';
 
 export default function HomePage() {
   const [code, setCode] = useState('');
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [, setLocation] = useLocation();
   useSetPageTitle('Home');
   return (
-    <Stack spacing={1}>
-      <Typography variant='h3' marginBottom={1}>
-        SolidPolls
-      </Typography>
-      <Typography variant='body1'>Please enter your poll code:</Typography>
-      <TextField
-        label='Poll code'
-        placeholder='12345678'
-        type='text'
-        variant='outlined'
-        onChange={(event) => setCode(event.target.value)}
-      />
-      <Link href={`/poll/${code}`}>
-        <Button variant='contained' type='submit'>
+    <>
+      <Snackbar
+        open={snackbarOpen}
+        onClose={() => setSnackbarOpen(false)}
+        autoHideDuration={6000}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+      >
+        <Alert
+          onClose={() => setSnackbarOpen(false)}
+          severity='error'
+          sx={{ width: '100%' }}
+        >
+          Poll not found.
+        </Alert>
+      </Snackbar>
+      <Stack spacing={2}>
+        <Typography variant='h3'>SolidPolls</Typography>
+        <Typography variant='body1'>Please enter your poll code:</Typography>
+        <TextField
+          label='Poll code'
+          placeholder='12345678'
+          type='text'
+          variant='outlined'
+          onChange={(event) => setCode(event.target.value)}
+        />
+        <Button
+          variant='contained'
+          type='submit'
+          onClick={() =>
+            pollsApi
+              .pollControllerFindByCode({ code })
+              .then((res) => setLocation(`/poll/${code}`))
+              .catch(() => setSnackbarOpen(true))
+          }
+        >
           Join
         </Button>
-      </Link>
-    </Stack>
+      </Stack>
+    </>
   );
 }
